3e66ca52b9bfa0fb69f4c0cabea0a4283271f35e
Naga bharathi <73803551+purecoder12@users.noreply.github.com>
2021-06-25 07:13:47 +0530

    Delete targetguidedordinal.py

--- targetguidedordinal.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df=pd.read_csv('C:/Users/Naga bharathi/Desktop/datasets/titanic_train.csv', usecols=['Cabin','Survived'])
-df.head()
-df['Cabin'].fillna('Missing',inplace=True)
-df['Cabin']=df['Cabin'].astype(str).str[0]
-print(df.head())
-print(df.groupby(['Cabin'])['Survived'].mean().sort_values().index)
